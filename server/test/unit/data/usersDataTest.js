7fbbf4e36045f9d8de7300b91a85cb74e4653daa
Unknown <mihailm956@gmail.com>
2017-07-30 16:38:29 +0300

    more unit tests

--- server/test/unit/data/usersDataTest.js
+++ server/test/unit/data/usersDataTest.js
@@ -1,36 +1,73 @@
-const { expect } = require('chai');
+// const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const sinon = require('sinon');
 
 const UsersData = require('../../../data/users.data');
 
-describe('UsersData.findByUsername', () => {
-
+describe('UsersData', () => {
     const db = {
         collection: () => { },
     };
-    let items = [];
+    const items = [{
+        name: 'Sasho',
+        username: 'SashoUsername',
+        email: 'SashoUsername@gmail.com',
+        password: 'sashoPass',
+        score: 0,
+    }, {
+        name: 'Gosho',
+        username: 'GoshoUsername',
+        email: 'GoshoUsername@gmail.com',
+        password: 'GoshoPass',
+        score: 0,
+    }];
     let data = null;
 
     const findOne = ({ username }) => {
-        return Promise.resolve(items.find((u) => u.username === username ));
+        return Promise.resolve(items.find((u) => u.username === username));
     };
 
-    describe('When we give first found User name as a searchName', () => {
-        beforeEach(() => {
-            items = [{
-                name: 'Sasho',
-                username: 'SashoUsername',
-                email: 'SashoUsername@gmail.com',
-                password: 'sashoPass',
-                score: 0,
-            }, {
-                name: 'Gosho',
-                username: 'GoshoUsername',
-                email: 'GoshoUsername@gmail.com',
-                password: 'GoshoPass',
-                score: 0,
-            }];
+    describe('.findByUsername', () => {
+        describe('When we give first found User name as a searchName', () => {
+            beforeEach(() => {
+                sinon.stub(db, 'collection')
+                    .callsFake(() => {
+                        return { findOne };
+                    });
 
+                data = new UsersData(db);
+            });
+
+            afterEach(() => {
+                db.collection.restore();
+            });
+
+            it('Expect to return the same user', () => {
+                const searchedUserName = items[0].username;
+
+                return data.findByUsername(searchedUserName)
+                    .then((foundUser) => {
+                        expect(foundUser.name).to.equal(items[0].name);
+                    });
+            });
+            // eslint-disable-next-line
+            it('Expect to throw if we compare the result with second found user`s name', () => {
+                const searchedUserName = items[0].username;
+
+                return data.findByUsername(searchedUserName)
+                    .then((foundUser) => {
+                        expect(foundUser.name).not.to.equal(items[1].name);
+                    });
+            });
+        });
+    });
+
+    describe('.checkPassword', () => {
+        beforeEach(() => {
             sinon.stub(db, 'collection')
                 .callsFake(() => {
                     return { findOne };
@@ -42,33 +79,73 @@ describe('UsersData.findByUsername', () => {
         afterEach(() => {
             db.collection.restore();
         });
-        
-        it('Expect to return the same user', () => {
-            const searchedUserName = items[0].username;
 
-            data.findByUsername(searchedUserName)
-                .then((foundUser) => {
-                    expect(foundUser.name).to.equal(items[0].name);
-                });
+        describe('When we give a valid/existing username and pass', () => {
+            it('Expect to return existing user', () => {
+                const expectedUsername = items[0].username;
+                const expectedPassword = items[0].password;
+                const expectedName = items[0].name;
+
+                return data.checkPassword(expectedUsername, expectedPassword)
+                    .then((foundUser) => {
+                        expect(foundUser.name).to.equal(expectedName);
+                    });
+            });
         });
 
-        it('Expect to throw if we compare the result with second found user`s name', () => {
-            const searchedUserName = items[0].username;
+        describe('When we give a wrong username', () => {
+            it('Expect to throw an error "Invalid user"', (done) => {
+                const wrongUsername = 'NoSuchUsername';
+                const expectedPassword = items[0].password;
+                const expectedName = items[0].name;
+                // eslint-disable-next-line
+                const promise = data.checkPassword(wrongUsername, expectedPassword);
 
-            data.findByUsername(searchedUserName)
-                .then((foundUser) => {
-                    expect(foundUser.name).not.to.equal(items[1].name);
-                });
+                promise
+                    .then(() => {
+                    }, (err) => {
+                        const msg = err.toString();
+                        expect((msg)).to.contain('Invalid user');
+                    })
+                .then(done, done);
+
+                // return expect(promise).to.eventually.throw('Invalid user');
+            });
         });
 
+        describe('When we give a wrong password', () => {
+            it('Expect to throw an error "Invalid password"', (done) => {
+                const expectedUsername = items[0].username;
+                const wrongPassword = 'wrongPass$$$';
+                const expectedName = items[0].name;
+                // eslint-disable-next-line
+                const promise = data.checkPassword(expectedUsername, wrongPassword);
 
-        it('Zashto tova se schita za veren test (ima zeleno tick-che)', () => {
-            const searchedUserName = items[0].username;
+                promise
+                    .then(() => {
+                    }, (err) => {
+                        const msg = err.toString();
+                        expect((msg)).to.contain('Invalid password');
+                    })
+                .then(done, done);
+                // eslint-disable-next-line
+                // return expect(promise).to.eventually.throw('Invalid password');
+            });
+        });
+    });
 
-            data.findByUsername(searchedUserName)
-                .then((foundUser) => {
-                    expect(foundUser.name).to.equal(items[1].name);
+    describe('.findById', () => {
+        beforeEach(() => {
+            sinon.stub(db, 'collection')
+                .callsFake(() => {
+                    return { findOne };
                 });
+
+            data = new UsersData(db);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
         });
     });
 });
--- server/test/unit/data/validator-tests.js
+++ server/test/unit/data/validator-tests.js
@@ -1,85 +1,85 @@
-const chai = require('chai');
-const expect = chai.expect;
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
+// const chai = require('chai');
+// const expect = chai.expect;
+// const chaiAsPromised = require('chai-as-promised');
+// chai.use(chaiAsPromised);
 
-const { validate } = require('../../../data/validator');
+// const { validate } = require('../../../data/validator');
 
-describe('validator', () => {
-    describe('validateRequiredInfo', () => {
-        it('expect to be rejected when Username is not provided', () => {
-            const user = {
-                username: '',
-                email: '',
-                password: '',
-            };
+// describe('validator', () => {
+//     describe('validateRequiredInfo', () => {
+//         it('expect to be rejected when Username is not provided', () => {
+//             const user = {
+//                 username: '',
+//                 email: '',
+//                 password: '',
+//             };
 
-            expect(validate(user)).to.be.rejectedWith('Username is required');
-        });
+//             expect(validate(user)).to.be.rejectedWith('Username is required');
+//         });
 
-        it('expect NOT to be rejected when Username is valid', () => {
-            const user = {
-                username: 'somename',
-                email: '',
-                password: '',
-            };
+//         it('expect NOT to be rejected when Username is valid', () => {
+//             const user = {
+//                 username: 'somename',
+//                 email: '',
+//                 password: '',
+//             };
 
-            expect(validate(user)).not.to.be.rejectedWith('Username is required');
-        });
-    });
+//             expect(validate(user)).not.to.be.rejectedWith('Username is required');
+//         });
+//     });
     
     
-    describe('validateLength', () => {
-        it('expect to be rejected when Username is with length less then 3', () => {
-            const user = {
-                username: 'so',
-                email: '',
-                password: '',
-            };
+//     describe('validateLength', () => {
+//         it('expect to be rejected when Username is with length less then 3', () => {
+//             const user = {
+//                 username: 'so',
+//                 email: '',
+//                 password: '',
+//             };
 
-            expect(validate(user)).to.be.rejectedWith('Username length must be between 3 and 30 symbols long');
-        });
+//             expect(validate(user)).to.be.rejectedWith('Username length must be between 3 and 30 symbols long');
+//         });
 
-        it('expect to be rejected when Username is with length more then 30', () => {
-            const user = {
-                username: '1234567890123456789012345678901234567890',
-                email: '',
-                password: '',
-            };
+//         it('expect to be rejected when Username is with length more then 30', () => {
+//             const user = {
+//                 username: '1234567890123456789012345678901234567890',
+//                 email: '',
+//                 password: '',
+//             };
 
-            expect(validate(user)).to.be.rejectedWith('Username length must be between 3 and 30 symbols long');
-        });
+//             expect(validate(user)).to.be.rejectedWith('Username length must be between 3 and 30 symbols long');
+//         });
         
-        it('expect NOT to be rejected when Username is with proper length', () => {
-            const user = {
-                username: 'somename',
-                email: '',
-                password: '',
-            };
+//         it('expect NOT to be rejected when Username is with proper length', () => {
+//             const user = {
+//                 username: 'somename',
+//                 email: '',
+//                 password: '',
+//             };
 
-            expect(validate(user)).not.to.be.rejectedWith('Username length must be between 3 and 30 symbols long');
-        });
-    });
+//             expect(validate(user)).not.to.be.rejectedWith('Username length must be between 3 and 30 symbols long');
+//         });
+//     });
 
-     describe('validateRegexMatch', () => {
-        it('expect to be rejected when email doesnt match a certain valid pattern', () => {
-            const user = {
-                username: 'someuser',
-                email: 'nonvalidemail@',
-                password: '',
-            };
+//      describe('validateRegexMatch', () => {
+//         it('expect to be rejected when email doesnt match a certain valid pattern', () => {
+//             const user = {
+//                 username: 'someuser',
+//                 email: 'nonvalidemail@',
+//                 password: '',
+//             };
 
-            expect(validate(user)).to.be.rejectedWith('Email doesnt match the tamplate *****@***.***');
-        });
+//             expect(validate(user)).to.be.rejectedWith('Email doesnt match the tamplate *****@***.***');
+//         });
 
-        it('expect NOT to be rejected when email matches a certain valid pattern', () => {
-            const user = {
-                username: 'someuser',
-                email: 'valid@email.com',
-                password: '',
-            };
+//         it('expect NOT to be rejected when email matches a certain valid pattern', () => {
+//             const user = {
+//                 username: 'someuser',
+//                 email: 'valid@email.com',
+//                 password: '',
+//             };
 
-            expect(validate(user)).not.to.be.rejectedWith('email doesnt match the tamplate *****@***.***');
-        });
-    });
-});
+//             expect(validate(user)).not.to.be.rejectedWith('email doesnt match the tamplate *****@***.***');
+//         });
+//     });
+// });
